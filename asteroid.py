a580b0508d907286200608be21c1cf014d45af9c
SampRession <bastsamp@outlook.com>
2025-01-26 15:40:43 +0100

    added split() method for asteroid instead of kill()
    Large asteroids will be splitted in 2 medium ones
    Medium asteroids will be splitted in 2 small ones
    Small asteroids will be destroyed

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
+import random
+
 import pygame as pg
 
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
@@ -12,3 +15,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        random_angle = random.uniform(20, 50)
+        new_pos_vector = self.velocity.rotate(random_angle)
+        new_neg_vector = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        splitted_ast_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        splitted_ast_2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        splitted_ast_1.velocity = new_pos_vector * 1.2
+        splitted_ast_2.velocity = new_neg_vector * 1.2
--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if ast.collision_check(shot):
-                    ast.kill()
+                    ast.split()
                     shot.kill()
 
         screen.fill("black")
